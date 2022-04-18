731fc003c56f3c206c91c76bd111e5dd00f14532
Tinkerer <46686760+TinkeringButReallyPlayingWithFire@users.noreply.github.com>
2022-04-18 18:05:06 +0100

    Salvation...

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,30 +26,10 @@ module.exports = {
       __key: "pages",
     },
     {
-      resolve: `gatsby-plugin-snipcart-advanced`,
+      resolve: "gatsby-plugin-snipcartv3",
       options: {
-        version: "3.0.29",
-        publicApiKey:
-          "ZjQ4MmRmMzItNmIyNi00ZjM5LWEzODEtYWI1ZDdjNDg4NTk1NjM3ODU4ODU3NjU2OTE4ODIx", // use public api key here or in environment variable
-        defaultLang: "en",
-        currency: "eur",
-        openCartOnAdd: false,
-        useSideCart: true,
-        // be careful with this mode cart. The cart in this mode has a bug of scroll in firefox
-        locales: {
-          fr: {
-            actions: {
-              checkout: "Valider le panier",
-            },
-          },
-        },
-        templatesUrl:
-          "path on your template file. Set file in the static folder, ex: '/snipcart/index.html'",
-        // not work on dev. Gatsby not serve html file in dev https://github.com/gatsbyjs/gatsby/issues/13072
-        innerHTML: `
-            <billing section="bottom">
-                <!-- Customization goes here -->
-            </billing>`,
+        apiKey:
+          "ZjQ4MmRmMzItNmIyNi00ZjM5LWEzODEtYWI1ZDdjNDg4NTk1NjM3ODU4ODU3NjU2OTE4ODIx",
       },
     },
   ],
