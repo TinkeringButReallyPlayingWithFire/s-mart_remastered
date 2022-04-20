111136e52d060fe04f7c9b292508a67cb142e73d
Tinkerer <46686760+TinkeringButReallyPlayingWithFire@users.noreply.github.com>
2022-04-20 17:40:37 +0100

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import Amplify, { Auth } from "aws-amplify";
-import awsConfig from "./src/aws-exports";
+import awsConfig from "./src/utils/aws-exports";
 const React = require("react");
 Amplify.configure(awsConfig);
