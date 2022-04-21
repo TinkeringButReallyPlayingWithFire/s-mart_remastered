702405d9ca07237f24fc5e2674b1529b520c9b7a
Tinkerer <46686760+TinkeringButReallyPlayingWithFire@users.noreply.github.com>
2022-04-21 11:21:45 +0100

    Amplify working integration :D

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "smartremastered6a7e731b": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,12 @@
-// import React from "react";
+import React from "react";
+import Amplify from "aws-amplify";
+import awsmobile from "../aws-exports";
+import { withAuthenticator, Authenticator } from "@aws-amplify/ui-react";
+import { AmplifyChatbot } from "@aws-amplify/ui-react/legacy";
+import "@aws-amplify/ui-react/styles.css";
+import { signContainer } from "../components/Profile/Profile.module.css";
+Amplify.configure(awsmobile);
+
 // import Homepage from "../components/Profile/Profile";
 // import { body } from "./index.module.css";
 
@@ -12,15 +20,38 @@
 // };
 
 // export default ProfilePage;
-import { withAuthenticator } from "aws-amplify-react";
 
 // markup
 const IndexPage = () => {
   return (
-    <div>
-      <h1>WOWOWOWOOWOWOWOW</h1>
-    </div>
+    <Authenticator
+      signUpAttributes={[
+        "birthdate",
+        "email",
+        "family_name",
+        "name",
+        "phone_number",
+      ]}
+    >
+      {({ signOut, user }) => (
+        <div className={signContainer}>
+          <div>
+            <h1>Hello {user.username}</h1>
+            {console.log("da user b from aws", user.attributes)}
+            <h4>
+              Name: <br />
+              {` ${user.attributes.name} ${user.attributes.family_name}`}
+            </h4>
+            <h4>Email address: {user.attributes.email}</h4>
+            <h4>Contact number: {user.attributes.phone_number}</h4>
+            <h4>Birthdate: {user.attributes.birthdate}</h4>
+
+            <button onClick={signOut}>Sign Out</button>
+          </div>
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
-export default withAuthenticator(IndexPage);
+export default IndexPage;
